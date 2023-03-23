5d2edf7769e8f016be966a87a4fc5cf5504f8537
YevhenBez <byxgalter@bigmir.net>
2023-03-23 13:50:10 +0200

    Написал функцию    fetchCountries(name)      которая должна делать     HTTP-запрос на      https://restcountries.com/#api-endpoints-v3-name    и возвращать Промис с массивом Стран.  И сразу же я сделал именованный Экспорт. Модуль 10
    Написал функцию    fetchCountries(name)      которая должна делать     HTTP-запрос на      https://restcountries.com/#api-endpoints-v3-name    и возвращать Промис с массивом Стран.  И сразу же я сделал именованный Экспорт (при помощи команды     export   ). При этом обрати внимание, что в конструкции     if     я использовал    !response.ok      и    throw new Error(response.status);     это необходимо, чтобы отловить ошибку 404, потому что     fetch     не ловит эту ошибку.   Модуль 10

---
+++ src/fetchCountries.js
@@ -0,0 +1,7 @@
+export const fetchCountries = name => fetch(`https://restcountries.com/v3.1/name/${name}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    });
