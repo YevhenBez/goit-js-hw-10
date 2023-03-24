dd7f5bd9a59b27b21418073a596298edc665c70c
YevhenBez <byxgalter@bigmir.net>
2023-03-24 21:28:50 +0200

    Добавил немного стилей в файле    styles.css      . В файле    fetchCountries.js       всё таки поставил      name     вместо     name.official     в запросе. В файле    index.js    прописал остальную логику. Модуль 10
    Добавил немного стилей в файле    styles.css      . В файле    fetchCountries.js       всё таки поставил      name     вместо     name.official     в запросе. В файле    index.js    прописал остальную логику. А именно - прописал функцию
     function alertTooManyMatches()    (когда в поле input    всего одна буква).   Прописал функцию     function alertWrongName()   когда в поле input     введена не существующая страна. Прописал функции     function renderCountryList(countries)      и      function renderCountryInfo(countries)    для разметки И задал условия вывода этих функций через if
     Модуль 10

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,4 +1,4 @@
-export const fetchCountries = name => fetch(`https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags,languages`)
+export const fetchCountries = name => fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
     .then(response => {
         if (!response.ok) {
             throw new Error(response.status);
--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const inputForm = document.querySelector('#search-box');
 
+const countryList = document.querySelector('.country-list')
+const countryInfo = document.querySelector('.country-info')
+
 const DEBOUNCE_DELAY = 300;
 
 const handleFormInput = event => {
@@ -15,10 +18,55 @@ const handleFormInput = event => {
 
     const seekedCoutry = event.target.value.trim();
     
+    if (seekedCoutry === '') {
+    return (countryList.innerHTML = ''), (countryInfo.innerHTML = '')
+    }
     
-    
-    fetchCountries(seekedCoutry).then(response => {
-        console.log(response);
-    }).catch(error => { console.log(error); })
+    fetchCountries(seekedCoutry).then(countries => {
+        countryList.innerHTML = ''
+        countryInfo.innerHTML = ''
+        if (countries.length === 1) {
+        countryList.insertAdjacentHTML('beforeend', renderCountryList(countries))
+        countryInfo.insertAdjacentHTML('beforeend', renderCountryInfo(countries))
+        } else if (countries.length >= 10) {
+        alertTooManyMatches()
+        } else {
+        countryList.insertAdjacentHTML('beforeend', renderCountryList(countries))
+        }
+    }).catch(alertWrongName)
 }
+function renderCountryList(countries) {
+    const markup = countries
+    .map(({ name, flags }) => {
+        return `
+            <li class="country-list__item">
+                
+                <h2 class="country-list__name"><img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>   ${name.official}</h2>
+            </li>
+            `
+    })
+    .join('')
+    return markup
+}
+function renderCountryInfo(countries) {
+    const markup = countries
+    .map(({ capital, population, languages }) => {
+        return `
+        <ul class="country-info__list">
+            <li class="country-info__item"><p><b>Capital: </b>${capital}</p></li>
+            <li class="country-info__item"><p><b>Population: </b>${population}</p></li>
+            <li class="country-info__item"><p><b>Languages: </b>${Object.values(languages).join(', ')}</p></li>
+        </ul>
+        `
+    })
+    .join('')
+    return markup
+}
+function alertWrongName() {
+    Notify.failure('Oops, there is no country with that name')
+}
+function alertTooManyMatches() {
+    Notify.info('Too many matches found. Please enter a more specific name.')
+}
+
 inputForm.addEventListener('input', debounce(handleFormInput, DEBOUNCE_DELAY));
