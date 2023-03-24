3c945b277f946043d5d597da3044847fc4f0d57e
YevhenBez <byxgalter@bigmir.net>
2023-03-24 11:04:41 +0200

    Написал функцию    handleFormInput       , которая должна выводить масивы стран при введении значения в     input     , а также нашёл значение, которое ввожу в input      (при помощи     seekedCoutry) Модуль 10
    Написал функцию    handleFormInput       , которая должна выводить массивы стран при введении значения в     input     , а также нашёл значение, которое ввожу в input      (при помощи     seekedCoutry). При этом я применил метод   trim()       , чтобы решить проблему, когда в поле введены только пробелы или они есть в начале и в конце рядка.   Модуль 10

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,4 +1,4 @@
-export const fetchCountries = name => fetch(`https://restcountries.com/v3.1/all?fields=name.official,capital,population,flags.svg,languages`)
+export const fetchCountries = name => fetch(`https://restcountries.com/v3.1/all?fields=${name}.official,capital,population,flags.svg,languages`)
     .then(response => {
         if (!response.ok) {
             throw new Error(response.status);
--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@ import { fetchCountries } from './fetchCountries';
 
 import debounce from 'lodash.debounce';
 
+const inputForm = document.querySelector('#search-box');
+
 const DEBOUNCE_DELAY = 300;
+
+const handleFormInput = event => {
+    event.preventDefault();
+
+    const seekedCoutry = event.target.value.trim();
+    
+    
+    
+    console.log(fetchCountries(seekedCoutry));
+
+}
+inputForm.addEventListener('input', handleFormInput);
